7527aa4f968fc4dd89981f4f0f461cd1828ba8a6
Ethan Mc Kelligan <jorgemckelligan@hotmail.com>
2024-02-08 15:34:16 -0600

    casi terminado el about-us

---
+++ about-us/main.js
@@ -0,0 +1,9 @@
+// Ubicamos los tres elementos del nav
+let primerBoton = document.querySelector('#quienes_somos')
+let segundoBoton = document.querySelector('#mision')
+let tercerBoton = document.querySelector('#vision')
+
+// Observers
+const observer = new IntersectionObserver()
+const segundoObserver = new IntersectionObserver()
+const tercerObserver = new IntersectionObserver()
